ccced669d132e8bbceef3bf3a28094f00bc14692
Nikolai Romanov <kolya.rm@gmail.com>
2026-02-02 20:59:44 +0100

    Updated episode card elements classes names. Added function creating episode card from template.

--- script.js
+++ script.js
@@ -7,6 +7,15 @@ function setup() {
 function makePageForEpisodes(episodeList) {
   // const rootElem = document.getElementById("root");
   // rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  const episodeCardTemplate = document.querySelector("#episode-card-template");
+  const rootElement = document.querySelector("#root");
+  
+  createEpisodeCard(rootElement, episodeCardTemplate, episodeList[0]);
+}
+
+function createEpisodeCard(parent, template, data) {
+  const card = template.content.cloneNode(true);
+  parent.append(card);
 }
 
 window.onload = setup;
